c25a1f69f8a6daba26d51c5e59f356861eae9b08
Jorge Aguilera <giito16@gmail.com>
2023-09-11 14:11:18 -0600

    :construction: first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularConfig'`, () => {
+  it(`should have as title 'angularTestCases'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularConfig');
+    expect(app.title).toEqual('angularTestCases');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'angularConfig app is running!'
+      'angularTestCases app is running!'
     );
   });
 });
